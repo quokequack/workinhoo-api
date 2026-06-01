aeb8a55d98084188f7cd37c28aaee78d431d6917
Maria Luísa Alaquoke <88796071+quokequack@users.noreply.github.com>
2026-05-31 22:16:06 -0300

    Update CORS paths to allow all endpoints

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'recuperar-senha/*', 'alterar'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
 
     'allowed_methods' => ['*'],
 
