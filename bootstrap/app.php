5396a81bfc0f312eddeb895e5532bb7c4ca4a296
Maria Luísa Alaquoke <88796071+quokequack@users.noreply.github.com>
2026-06-02 18:53:50 -0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
         then: function () {
-            Route::middleware(['api', 'auth:sanctum'])
+            Route::middleware(['api', 'auth:sanctum', 'web'])
                 ->prefix('api')
                 ->group(base_path('routes/auth.php'));
         },
