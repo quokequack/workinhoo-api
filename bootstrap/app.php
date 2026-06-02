2e52a2a294351d29977097992319052104e37eed
Maria Luísa Alaquoke <88796071+quokequack@users.noreply.github.com>
2026-06-02 18:14:29 -0300

    Add web middleware with session support (#22)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
+        $middleware->web(append: [
+        StartSession::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
